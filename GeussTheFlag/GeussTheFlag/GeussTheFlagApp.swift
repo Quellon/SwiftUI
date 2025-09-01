1479e7141ec71f83b26edd6442475c61b12e7f50
Quellon Naicker <quellon@mad9000.com>
2025-09-01 13:50:16 +0200

    Add files via upload

---
+++ GeussTheFlag/GeussTheFlag/GeussTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeussTheFlagApp.swift
+//  GeussTheFlag
+//
+//  Created by Quellon Naicker on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct GeussTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
